19098f2f2fdef2ee3a62d83a55bbb6243f79d0e8
Matthias Schulte <post@lupo49.de>
2013-03-29 12:39:15 +0100

    refactored to DokuWikis jQuery mode

--- script.js
+++ script.js
@@ -1,42 +1,35 @@
 /*
-    enforcesummary plugin for DokuWiki
-    File: script.js
-    http://www.dokuwiki.org/plugin:enforcesummary
-*/
+ enforcesummary plugin for DokuWiki
+ File: script.js
+ http://www.dokuwiki.org/plugin:enforcesummary
+ */
 
-function installSummaryEnforcement() {
-    var summary_input = document.getElementById('edit__summary'); // get summary field 
-    
-    if(summary_input !== null) {
-        var minoredit_input = document.getElementById('minoredit');
- 
-        addEvent(summary_input, 'change', enforceSummary);
-        addEvent(summary_input, 'keyup', enforceSummary);
-        addEvent(minoredit_input, 'change', enforceSummary);
-        addEvent(minoredit_input, 'click', enforceSummary);
-        enforceSummary(); // summary may be there if we're previewing
-    }
-}
- 
+jQuery(function() {
+    var $summary = jQuery('#edit__summary'); // get summary field
+    var $minoredit = jQuery('#minoredit');
+
+    // Parts copied from https://www.dokuwiki.org/tips:summary_enforcement
+    $summary.keyup(enforceSummary).focus(enforceSummary);
+    $minoredit.change(enforceSummary);
+    enforceSummary(); // summary may be there if we're previewing
+})
+
 function enforceSummary() {
-    var btn_save = document.getElementById('edbtn__save');
-    var summary_input = document.getElementById('edit__summary');
-    var minoredit_input = document.getElementById('minoredit');
-    var disabled = false;
- 
+    var $summary = jQuery('#edit__summary'); // get summary field
+    var $minoredit = jQuery('#minoredit');
+    var $editbutton = jQuery("#edbtn__save");
+
     // always require summary, even when minor change option is checked
-    // if(summary_input.value.replace(/^\s+/,"") === '') 
-    
-    if(summary_input.value.replace(/^\s+/,"") === '' && !minoredit_input.checked) {
-        disabled = true;
-    }
- 
-    if(disabled != btn_save.disabled || btn_save.disabled === null) {
-        btn_save.className = disabled ? 'button button_disabled' : 'button';
-        btn_save.disabled = disabled;
+    // if($summary.val().replace(/^\s+/,"") === '')
+
+    if(typeof $summary.val() != 'undefined') {
+        if($summary.val().replace(/^\s+/,'') === '' && !$minoredit.is(':checked')) {
+            $summary.addClass("missing");
+            $editbutton.attr("disabled", true).css("color", "#999");
+        } else {
+            $summary.removeClass("missing");
+            $editbutton.removeAttr("disabled").css("color", "black");
+        }
     }
 }
- 
-addInitEvent(function() {
-        installSummaryEnforcement();
-    });
+
