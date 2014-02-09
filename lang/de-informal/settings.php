5964f5e89a838d05414d4a62089b48ba8a359c7e
Matthias Schulte <dokuwiki@lupo49.de>
2014-02-09 18:37:22 +0100

    de/de-informal: language updates

---
+++ lang/de-informal/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * German language file for EnforceSummary Plugin
+ * used in the configuration manager
+ */
+
+$lang['enforce_summary'] = "Erzwinge Wert für Zusammenfassung";
+$lang['default_minoredit'] = "Kleine Änderung standardmäßig aktiviert";
+$lang['enforce_preview'] = "Vorschau erzwingen, bevor die Seite gespeichert werden kann";
