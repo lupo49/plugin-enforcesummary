c7fe70266ea9347e67980c8f7ecfed1ea0d13798
Satoshi Sahara <sahara.satoshi@gmail.com>
2020-10-24 21:46:54 +0900

    PSR-12 coding style

--- action.php
+++ action.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * DokuWiki Action component of EnforceSummary Plugin
  *
@@ -6,42 +7,34 @@
  * @author  Matthias Schulte <dokuwiki@lupo49.de>
  * @author  Sahara Satoshi <sahara.satoshi@gmail.com>
  */
-// must be run within Dokuwiki
-if(!defined('DOKU_INC')) die();
-
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC.'lib/plugins/');
-require_once(DOKU_PLUGIN.'action.php');
-
-/**
- * All DokuWiki plugins to interfere with the event system
- * need to inherit from this class
- */
-class action_plugin_enforcesummary extends DokuWiki_Action_Plugin {
-
+class action_plugin_enforcesummary extends DokuWiki_Action_Plugin
+{
     // register hook
-    function register(Doku_Event_Handler $controller) {
-        $controller->register_hook('DOKUWIKI_STARTED', 'AFTER', $this, '_exportToJSINFO');
-        $controller->register_hook('HTML_EDITFORM_OUTPUT', 'BEFORE', $this, '_append_edit_guide');
+    public function register(Doku_Event_Handler $controller)
+    {
+        $controller->register_hook('DOKUWIKI_STARTED', 'AFTER', $this, 'exportToJSINFO');
+        $controller->register_hook('HTML_EDITFORM_OUTPUT', 'BEFORE', $this, 'appendEditGuide');
     }
 
     /**
      * Exports configuration settings to $JSINFO
      */
-    function _exportToJSINFO(&$event) {
-
+    public function exportToJSINFO(Doku_Event $event)
+    {
         global $JSINFO;
 
         $JSINFO['plugin_enforcesummary'] = array(
                 'enforce_summary'    => $this->getConf('enforce_summary'),
                 'default_minoredit'  => $this->getConf('default_minoredit'),
                 'enforce_preview'    => $this->getConf('enforce_preview'),
-            );
+        );
     }
 
     /**
      * Append Edit Guide in the Edit Window (below save button)
      */
-    function _append_edit_guide(&$event) {
+    public function appendEditGuide(Doku_Event $event)
+    {
         $pos = $event->data->findElementByAttribute('class', 'editButtons');
         if (!$pos) return; // no editButtons
         $guidance = $this->locale_xhtml('edit_guide');
