de257808cfd8e0d59e4d230c89249fd23159aa77
Gregory Loichot <gregory.loichot@unige.ch>
2024-04-16 13:52:45 +0200

    Refactoring 2020
    Cleanup code according to https://www.dokuwiki.org/devel:releases:refactor2020#general_cleanup_suggestions to remove deprecated log.

--- action.php
+++ action.php
@@ -9,9 +9,6 @@
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
 
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC.'lib/plugins/');
-require_once(DOKU_PLUGIN.'action.php');
-
 /**
  * All DokuWiki plugins to interfere with the event system
  * need to inherit from this class
