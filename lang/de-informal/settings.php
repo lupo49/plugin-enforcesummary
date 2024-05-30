279150220fd3deb907b7dbcf77b3679943b73a13
Michael Hamann <michael.hamann@xwiki.com>
2024-05-30 14:05:33 +0200

    Fix #7 encoding of German translations

--- lang/de-informal/settings.php
+++ lang/de-informal/settings.php
@@ -4,6 +4,6 @@
  * used in the configuration manager
  */
 
-$lang['enforce_summary'] = "Erzwinge Wert für Zusammenfassung";
-$lang['default_minoredit'] = "Kleine Änderung standardmäßig aktiviert";
+$lang['enforce_summary'] = "Erzwinge Wert fÃ¼r Zusammenfassung";
+$lang['default_minoredit'] = "Kleine Ã„nderung standardmÃ¤ÃŸig aktiviert";
 $lang['enforce_preview'] = "Vorschau erzwingen, bevor die Seite gespeichert werden kann";
